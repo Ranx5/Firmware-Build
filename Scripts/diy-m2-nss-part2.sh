084ed65f509437fe838eb23035f3416af0457463
Ran <63771479+Ranx5@users.noreply.github.com>
2024-05-31 16:47:40 +0800

    Update diy-m2-nss-part2.sh

--- Scripts/diy-m2-nss-part2.sh
+++ Scripts/diy-m2-nss-part2.sh
@@ -11,11 +11,26 @@ git clone --depth=1 https://github.com/vernesong/OpenClash.git -b dev package/lu
 # 添加luci-app-argon
 git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon.git -b master package/luci-theme-argon
 git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config.git -b master package/luci-app-argon-config
-# 修改python
-# sudo mkdir python
-# sudo chmod -R 777 python
-# git clone https://github.com/immortalwrt/packages.git python && cd python && git checkout c2904e115185493d3429e7e7c15e0752ce76a35f
-# cd ..
-# sudo rm -rf feeds/packages/lang/python
-# cp -rf python/lang/python feeds/packages/lang/python
-# sudo rm -rf python
+# 修改dst
+wget -cO target/linux/qualcommax/files/arch/arm64/boot/dts/qcom/ipq6018-ax18.dts https://raw.githubusercontent.com/Ranx5/Firmware-Build/main/Defconfig/ipq6000/ipq6018-ax18.dts
+wget -cO target/linux/qualcommax/files/arch/arm64/boot/dts/qcom/ipq6018-cmiot.dtsi https://raw.githubusercontent.com/Ranx5/Firmware-Build/main/Defconfig/ipq6000/ipq6018-cmiot.dtsi
+wget -cO target/linux/qualcommax/files/arch/arm64/boot/dts/qcom/ipq6018-cpufreq.dtsi https://raw.githubusercontent.com/Ranx5/Firmware-Build/main/Defconfig/ipq6000/ipq6018-cpufreq.dtsi
+wget -cO target/linux/qualcommax/files/arch/arm64/boot/dts/qcom/ipq6018-m2.dts https://raw.githubusercontent.com/Ranx5/Firmware-Build/main/Defconfig/ipq6000/ipq6018-m2.dts
+rm -f target/linux/qualcommax/image/ipq60xx.mk
+wget -cO target/linux/qualcommax/image/ipq60xx.mk https://raw.githubusercontent.com/Ranx5/Firmware-Build/main/Defconfig/ipq6000/ipq60xx.mk
+wget -cO package/firmware/ipq-wifi/src/board-cmiot_ax18.ipq6018 https://github.com/Ranx5/Firmware-Build/blob/main/Defconfig/ipq6000/board-cmiot_ax18.ipq6018
+wget -cO package/firmware/ipq-wifi/src/board-zn_m2.ipq6018 https://github.com/Ranx5/Firmware-Build/blob/main/Defconfig/ipq6000/board-zn_m2.ipq6018
+rm -f package/firmware/ipq-wifi/Makefile
+wget -cO package/firmware/ipq-wifi/Makefile https://raw.githubusercontent.com/Ranx5/Firmware-Build/main/Defconfig/ipq6000/Makefile
+rm -f package/boot/uboot-envtools/files/qualcommax_ipq60xx
+wget -cO package/boot/uboot-envtools/files/qualcommax_ipq60xx https://raw.githubusercontent.com/Ranx5/Firmware-Build/main/Defconfig/ipq6000/qualcommax_ipq60xx
+rm -f target/linux/qualcommax/ipq60xx/base-files/etc/board.d/01_leds
+wget -cO target/linux/qualcommax/ipq60xx/base-files/etc/board.d/01_leds https://raw.githubusercontent.com/Ranx5/Firmware-Build/main/Defconfig/ipq6000/01_leds
+rm -f target/linux/qualcommax/ipq60xx/base-files/etc/board.d/02_network
+wget -cO target/linux/qualcommax/ipq60xx/base-files/etc/board.d/02_network https://raw.githubusercontent.com/Ranx5/Firmware-Build/main/Defconfig/ipq6000/02_network
+rm -f target/linux/qualcommax/ipq60xx/base-files/etc/hotplug.d/firmware/11-ath11-caldata
+wget -cO target/linux/qualcommax/ipq60xx/base-files/etc/hotplug.d/firmware/11-ath11-caldata https://raw.githubusercontent.com/Ranx5/Firmware-Build/main/Defconfig/ipq6000/11-ath11-caldata
+rm -f target/linux/qualcommax/ipq60xx/base-files/lib/upgrade/platform.sh
+wget -cO target/linux/qualcommax/ipq60xx/base-files/lib/upgrade/platform.sh https://raw.githubusercontent.com/Ranx5/Firmware-Build/main/Defconfig/ipq6000/platform.sh
+
+
