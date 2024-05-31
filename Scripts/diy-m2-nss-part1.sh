339d02f511479c5ea5d49636a1ab353faeab129b
Ran <63771479+Ranx5@users.noreply.github.com>
2024-05-31 17:39:08 +0800

    Update diy-m2-nss-part1.sh

--- Scripts/diy-m2-nss-part1.sh
+++ Scripts/diy-m2-nss-part1.sh
@@ -2,9 +2,9 @@
 # Add a feed source
 # echo 'src-git passwall2_packages https://github.com/xiaorouji/openwrt-passwall-packages.git' >> feeds.conf.default
 # echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git' >> feeds.conf.default
-sudo mkdir tmp && sudo chmod -R 777 tmp
-git clone --depth=1  https://github.com/immortalwrt/immortalwrt.git -b master tmp
-sudo cp -rf tmp/package/emortal package/emortal
-sudo rm -rf tmp
+# sudo mkdir tmp && sudo chmod -R 777 tmp
+# git clone --depth=1  https://github.com/immortalwrt/immortalwrt.git -b master tmp
+# sudo cp -rf tmp/package/emortal package/emortal
+# sudo rm -rf tmp
 ./scripts/feeds update -a
 ./scripts/feeds install -a
