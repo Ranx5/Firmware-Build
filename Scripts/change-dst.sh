3d162ca0c59fdb676ca6c7ba915fac93974c9a82
Ran <63771479+Ranx5@users.noreply.github.com>
2024-06-04 22:12:56 +0800

    Update and rename diy-imm6-part2.sh to change-dst.sh

--- Scripts/change-dst.sh
+++ Scripts/change-dst.sh
@@ -1,22 +1,5 @@
 
 #!/bin/bash
-# 修改默认IP
-sed -i 's/192.168.1.1/192.168.32.1/g' package/base-files/files/bin/config_generate
-# 添加luci-app-mosdns
-rm -rf feeds/packages/net/v2ray-geodata
-git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
-git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
-old_perms=$(stat -c %a package/mosdns/luci-app-mosdns/root/etc/init.d/mosdns)
-echo $old_perms
-rm -f package/mosdns/luci-app-mosdns/root/etc/init.d/mosdns
-wget -cO package/mosdns/luci-app-mosdns/root/etc/init.d/mosdns https://raw.githubusercontent.com/Ranx5/Firmware-Build/main/Configs/mosdns
-chmod $old_perms package/mosdns/luci-app-mosdns/root/etc/init.d/mosdns
-# 添加luci-app-openclash
-rm -rf feeds/luci/applications/luci-app-openclash
-git clone --depth=1 https://github.com/vernesong/OpenClash.git -b dev package/luci-app-openclash
-# 添加luci-app-passwall2
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall-packages
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2.git package/luci-app-passwall2
 # 更换dst
 rm -f package/boot/uboot-envtools/files/mediatek_filogic
 wget -cO package/boot/uboot-envtools/files/mediatek_filogic https://raw.githubusercontent.com/Ranx5/Firmware-Build/main/Defconfig/rax3000m/mediatek_filogic
