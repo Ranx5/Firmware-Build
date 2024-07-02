0e06a8f34e951533e752f6c9fed72c7ea2c2bf99
Ran <63771479+Ranx5@users.noreply.github.com>
2024-07-02 11:16:55 +0800

    Update diy-798x-part2.sh

--- Scripts/diy-798x-part2.sh
+++ Scripts/diy-798x-part2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # 修改默认IP
-sed -i 's/192.168.1.1/192.168.32.1/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/192.168.32.1/g' package/base-files/files/bin/config_generate
 # 添加luci-app-mosdns
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 20.x feeds/packages/lang/golang
