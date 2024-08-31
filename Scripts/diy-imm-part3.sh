cfea79e0066756fa8e33fbb6e5757804f1bc2bc4
Ran <63771479+Ranx5@users.noreply.github.com>
2024-08-31 10:45:50 +0800

    Update diy-imm-part3.sh

--- Scripts/diy-imm-part3.sh
+++ Scripts/diy-imm-part3.sh
@@ -13,9 +13,6 @@ chmod $old_perms package/mosdns/luci-app-mosdns/root/etc/init.d/mosdns
 # 添加luci-app-openclash
 rm -rf feeds/luci/applications/luci-app-openclash
 git clone --depth=1 https://github.com/vernesong/OpenClash.git -b dev package/luci-app-openclash
-# 添加luci-app-passwall2
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall-packages
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2.git package/luci-app-passwall2
 # 添加luci-app-mihomo
 git clone --depth=1 https://github.com/morytyann/OpenWrt-mihomo.git -b main package/luci-app-mihomo
 # 更换Ruby
