3da3291a01e667421661cd4f824481d7b26e671d
Ran <63771479+Ranx5@users.noreply.github.com>
2024-08-31 09:48:49 +0800

    Update perset-mihomo.sh

--- Scripts/perset-mihomo.sh
+++ Scripts/perset-mihomo.sh
@@ -6,8 +6,9 @@ mkdir -p files/usr/bin
 mkdir -p files/etc/mihomo
 
 # 设置下载链接
-CONFIG_MODIFY_URL="https://raw.githubusercontent.com/Ranx5/Firmware-Build/main/Defconfig/modify_uv.py"
+CONFIG_MODIFY_URL="https://raw.githubusercontent.com/Ranx5/Firmware-Build/main/Defconfig/mihomo_uv.py"
 MOSDNS_CONFIG_URL="https://raw.githubusercontent.com/Ranx5/Firmware-Build/main/Configs/mosdns.config"
+MIHOMO_CONFIG_URL="https://raw.githubusercontent.com/Ranx5/Firmware-Build/main/Configs/mihomo.config"
 
 # 获取仓库的最新发布信息
 release_info=$(wget -qO- https://api.github.com/repos/MetaCubeX/mihomo/releases/latest)
@@ -32,8 +33,10 @@ fi
 # 下载配置文件
 wget -qO- $CONFIG_MODIFY_URL > files/etc/mihomo/modify_uv.py
 wget -qO- $MOSDNS_CONFIG_URL > files/etc/config/mosdns
+wget -qO- $MIHOMO_CONFIG_URL > files/etc/config/mihomo
 
 # 设置权限
 chmod +x files/usr/bin/mihomo
-chmod +x files/etc/openclash/modify_uv.py
+chmod +x files/etc/mihomo/modify_uv.py
 chmod +rw files/etc/config/mosdns
+chmod +rw files/etc/config/mihomo
