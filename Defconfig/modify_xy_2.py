0f69e64157f71e0dcee856ad452d38084898f14e
Ran <63771479+Ranx5@users.noreply.github.com>
2024-09-15 11:03:21 +0800

    Update modify_xy_2.py

--- Defconfig/modify_xy_2.py
+++ Defconfig/modify_xy_2.py
@@ -173,6 +173,7 @@ z['sniffer'] = {
             'Mijia Cloud'
         ]
     }
+z['dns'] = {'proxy-server-nameserver': ['223.5.5.5']}
 if os.path.exists('/etc/openclash/config/config_xy.yaml'):
     os.system('rm /etc/openclash/config/config_xy.yaml')
 with open('/etc/openclash/config/config_xy.yaml', 'w') as file:
