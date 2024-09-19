78ef5dc9ca0a7981f6a5e853e55333f830964ef9
Ran <63771479+Ranx5@users.noreply.github.com>
2024-09-19 19:34:03 +0800

    Update diy-imm-part3.sh

--- Scripts/diy-imm-part3.sh
+++ Scripts/diy-imm-part3.sh
@@ -15,6 +15,9 @@ rm -rf feeds/luci/applications/luci-app-openclash
 git clone --depth=1 https://github.com/vernesong/OpenClash.git -b dev package/luci-app-openclash
 # 添加luci-app-mihomo
 git clone --depth=1 https://github.com/morytyann/OpenWrt-mihomo.git -b main package/luci-app-mihomo
+# 添加luci-app-passwall2
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2.git -b main package/luci-app-passwall2
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages.git -b main package/passwall-packages
 # 更换Ruby
 rm -rf feeds/packages/lang/ruby
 mkdir ruby && chmod -R 777 ruby
