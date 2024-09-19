0c5c22da6c8126e808ffdd90027dfcdd0671e987
Ran <63771479+Ranx5@users.noreply.github.com>
2024-09-19 21:17:32 +0800

    Update diy-imm-part2.sh

--- Scripts/diy-imm-part2.sh
+++ Scripts/diy-imm-part2.sh
@@ -16,5 +16,5 @@ git clone --depth=1 https://github.com/vernesong/OpenClash.git -b dev package/lu
 # 添加luci-app-mihomo
 git clone --depth=1 https://github.com/morytyann/OpenWrt-mihomo.git -b main package/luci-app-mihomo
 # 添加luci-app-passwall2
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2.git -b main package/luci-app-passwall2
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages.git -b main package/passwall-packages
+# git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2.git -b main package/luci-app-passwall2
+# git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages.git -b main package/passwall-packages
