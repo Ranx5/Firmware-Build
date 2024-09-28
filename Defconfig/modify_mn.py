0d2f84174cf43fb060d9e69381f5eacfe7c1ccd1
Ran <63771479+Ranx5@users.noreply.github.com>
2024-09-28 13:43:03 +0800

    Update modify_mn.py

--- Defconfig/modify_mn.py
+++ Defconfig/modify_mn.py
@@ -66,62 +66,39 @@ for s in ProxySet:
     if s != 'OT':
         pgs.append({'name':s, 'type': 'load-balance', 'strategy': 'consistent-hashing', 'disable-udp': False,
                     'proxies':Proxy[s], 'url': 'http://www.gstatic.com/generate_204', 'interval': testtime})
+pgs.append({'name':'DNS', 'type':'select', 'proxies':ProxySet+Proxy['All']})
 if Proxy['OT']:
     pgs.append({'name':'OT', 'type': 'select', 'proxies':Proxy['OT']})
     
 rps = {}
-rps['Google'] = {'type': 'http', 'behavior': 'classical', 'path':'./rule_provider/Google.yaml',
-                           'url':'https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/Providers/Ruleset/Google.yaml'}
-rps['Youtube'] = {'type': 'http', 'behavior': 'classical', 'path':'./rule_provider/Youtube.yaml',
-                           'url':'https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/Providers/Ruleset/YouTube.yaml'}
-rps['Disneyplus'] = {'type': 'http', 'behavior': 'classical', 'path':'./rule_provider/Disneyplus.yaml',
-                           'url':'https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/Providers/Ruleset/DisneyPlus.yaml'}
-rps['Netflix'] = {'type': 'http', 'behavior': 'classical', 'path':'./rule_provider/Netflix.yaml',
-                           'url':'https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/Providers/Ruleset/Netflix.yaml'}
-rps['Instagram'] = {'type': 'http', 'behavior': 'classical', 'path':'./rule_provider/Instagram.yaml',
-                           'url':'https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/Providers/Ruleset/Instagram.yaml'}
-rps['Facebook'] = {'type': 'http', 'behavior': 'classical', 'path':'./rule_provider/Facebook.yaml',
-                           'url':'https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/Providers/Ruleset/Facebook.yaml'}
-rps['Spotify'] = {'type': 'http', 'behavior': 'classical', 'path':'./rule_provider/Spotify.yaml',
-                           'url':'https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/Providers/Ruleset/Spotify.yaml'}
-rps['Pornhub'] = {'type': 'http', 'behavior': 'classical', 'path':'./rule_provider/Spotify.yaml',
-                           'url':'https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/Providers/Ruleset/Pornhub.yaml'}
-rps['Github'] = {'type': 'http', 'behavior': 'classical', 'path':'./rule_provider/Github.yaml',
-                           'url':'https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/Providers/Ruleset/Github.yaml'}
-rps['Twitter'] = {'type': 'http', 'behavior': 'classical', 'path':'./rule_provider/Twitter.yaml',
-                           'url':'https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/Providers/Ruleset/Twitter.yaml'}
-rps['Telegram'] = {'type': 'http', 'behavior': 'ipcidr', 'path':'./rule_provider/Telegram.yaml',
-                           'url':'https://raw.githubusercontent.com/Loyalsoldier/clash-rules/release/telegramcidr.txt'}
-rps['NetflixIP'] = {'type': 'http', 'behavior': 'classical', 'path':'./rule_provider/NetflixIP.yaml',
-                           'url':'https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/Providers/Ruleset/NetflixIP.yaml'}
-rps['Microsoft'] = {'type': 'http', 'behavior': 'classical', 'path':'./rule_provider/Microsoft.yaml',
-                           'url':'https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/Providers/Ruleset/Microsoft.yaml'}
-rps['OpenAI'] = {'type': 'http', 'behavior': 'classical', 'path':'./rule_provider/OpenAI.yaml',
-                           'url':'https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/Providers/Ruleset/OpenAi.yaml'}
-rps['Scholar'] = {'type': 'http', 'behavior': 'classical', 'path':'./rule_provider/Scholar.yaml',
-                           'url':'https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/Providers/Ruleset/Scholar.yaml'}
 rps['ProxyGFW'] = {'type': 'http', 'behavior': 'classical', 'path':'./rule_provider/ProxyGFW.yaml',
                            'url':'https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/Providers/ProxyGFWlist.yaml'}
+
 rs = []
-rs.append('RULE-SET,Telegram,Telegram')
-rs.append('RULE-SET,Twitter,Twitter')
-rs.append('RULE-SET,Instagram,Instagram')
-rs.append('RULE-SET,Facebook,Instagram')
-rs.append('RULE-SET,Youtube,YouTube')
+rs.append('GEOIP,private,DIRECT,no-resolve')
+rs.append('IP-SUFFIX,1.1.1.1/24,DNS,no-resolve')
+rs.append('IP-SUFFIX,8.8.8.8/24,DNS,no-resolve')
+rs.append('GEOIP,telegram,Telegram,no-resolve')
+rs.append('GEOSITE,twitter,Twitter')
+rs.append('GEOIP,twitter,Twitter,no-resolve')
+rs.append('GEOSITE,instagram,Instagram')
+rs.append('GEOSITE,facebook,Instagram')
+rs.append('GEOSITE,youtube,YouTube')
 rs.append('DOMAIN-SUFFIX,googleapis.cn,Google')
-rs.append('RULE-SET,Google,Google')
-rs.append('RULE-SET,Spotify,Spotify')
-rs.append('RULE-SET,Github,GitHub')
-rs.append('RULE-SET,OpenAI,OpenAI')
-rs.append('RULE-SET,Microsoft,Microsoft')
-rs.append('RULE-SET,Disneyplus,DisneyPlus')
-rs.append('RULE-SET,Netflix,Netflix')
-rs.append('RULE-SET,NetflixIP,Netflix')
-rs.append('RULE-SET,Scholar,DIRECT')
-rs.append('RULE-SET,Pornhub,PornHub')
+rs.append('GEOSITE,google,Google')
+rs.append('GEOSITE,spotify,Spotify')
+rs.append('GEOSITE,github,GitHub')
+rs.append('GEOSITE,openai,OpenAI')
+rs.append('GEOSITE,microsoft,Microsoft')
+rs.append('GEOSITE,disney,DisneyPlus')
+rs.append('GEOSITE,netflix,Netflix')
+rs.append('GEOIP,netflix,Netflix,no-resolve')
+rs.append('GEOSITE,category-scholar-cn,DIRECT')
 rs.append('RULE-SET,ProxyGFW,Proxy')
 rs.append('GEOIP,CN,DIRECT,no-resolve')
 rs.append('GEOSITE,apple,DIRECT')
+rs.append('DOMAIN-SUFFIX,ip.sb,GitHub')
+rs.append('DOMAIN-SUFFIX,ipify.org,YouTube')
 rs.append('MATCH,Proxy')
 z = {}
 for k in x.keys():
