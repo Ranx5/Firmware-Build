f10fac850de2e5f3b3d6d160be8ee2c5ff981464
Ran <63771479+Ranx5@users.noreply.github.com>
2024-10-19 09:39:31 +0800

    Update diy-imm-part1.sh

--- Scripts/diy-imm-part1.sh
+++ Scripts/diy-imm-part1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # 修改默认IP
-sed -i 's/192.168.1.1/192.168.32.10/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/192.168.32.10/g' package/base-files/files/bin/config_generate
 # 更新feed
 ./scripts/feeds update -a
 ./scripts/feeds install -a
