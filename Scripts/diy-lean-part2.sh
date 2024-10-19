869f8a02686a75c9e69e5c29df90035affaa31eb
Ran <63771479+Ranx5@users.noreply.github.com>
2024-10-19 09:07:19 +0800

    Update diy-lean-part2.sh

--- Scripts/diy-lean-part2.sh
+++ Scripts/diy-lean-part2.sh
@@ -10,8 +10,21 @@ find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
 find ./ | grep Makefile | grep mosdns | xargs rm -f
 git clone --depth=1 https://github.com/sbwml/luci-app-mosdns.git -b v5-lua package/mosdns
 git clone --depth=1 https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+old_perms=$(stat -c %a package/mosdns/luci-app-mosdns/root/etc/init.d/mosdns)
+echo $old_perms
+rm -f package/mosdns/luci-app-mosdns/root/etc/init.d/mosdns
+wget -cO package/mosdns/luci-app-mosdns/root/etc/init.d/mosdns https://raw.githubusercontent.com/Ranx5/Firmware-Build/main/Configs/mosdns
+chmod $old_perms package/mosdns/luci-app-mosdns/root/etc/init.d/mosdns
 # 添加luci-app-openclash
 git clone --depth=1 https://github.com/vernesong/OpenClash.git -b dev package/luci-app-openclash
 # 添加luci-theme-argone
 git clone --depth=1 https://github.com/kenzok78/luci-theme-argone.git -b main package/luci-theme-argone
 git clone --depth=1 https://github.com/kenzok78/luci-app-argone-config.git -b main package/luci-app-argone-config
+# 添加luci-app-passwall
+rm -rf feeds/luci/applications/luci-app-passwall
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall.git -b main package/luci-app-passwall
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages.git -b main package/passwall-packages
+# 添加luci-app-passwall2
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2.git -b main package/luci-app-passwall2
+# 添加luci-app-amlogic
+git clone --depth=1 https://github.com/ophub/luci-app-amlogic.git -b main package/luci-app-amlogic
