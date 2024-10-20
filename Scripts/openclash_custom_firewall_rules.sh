47eb6a55d9bc692bdd39e9c83a9b71913b5b2dee
Ran <63771479+Ranx5@users.noreply.github.com>
2024-10-20 22:41:27 +0800

    Update openclash_custom_firewall_rules.sh

--- Scripts/openclash_custom_firewall_rules.sh
+++ Scripts/openclash_custom_firewall_rules.sh
@@ -6,6 +6,6 @@
 # Add your custom firewall rules here, they will be added after the end of the OpenClash iptables rules
 
 LOG_OUT "Tip: Start Add Custom Firewall Rules..."
-curl -s https://anti-ad.net/anti-ad-for-dnsmasq.conf -o /etc/openclash/dnsmasq.d/anti-ad-for-dnsmasq.conf
+curl -s https://anti-ad.net/anti-ad-for-dnsmasq.conf -o /etc/openclash/anti-ad-for-dnsmasq.conf
 /etc/init.d/dnsmasq restart
 exit 0
