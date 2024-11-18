9622ebb3bef66af5bd000d70065cdb84f428562e
Ran <63771479+Ranx5@users.noreply.github.com>
2024-11-18 20:15:01 +0800

    Delete Scripts/99z-default-setting.sh

--- Scripts/99z-default-setting.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-uci add_list dhcp.@dnsmasq[0].addnhosts='/etc/openclash/anti-ad-for-dnsmasq.conf'
-uci commit dhcp
-/etc/init.d/dnsmasq restart
-uci set openclash.config.dashboard_password='123456'
-uci commit openclash
