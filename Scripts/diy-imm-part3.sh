c6a6ad6b1bc765aee2a6d6cc4551ae2a3422e068
Ran <63771479+Ranx5@users.noreply.github.com>
2024-11-20 21:00:30 +0800

    Update diy-imm-part3.sh

--- Scripts/diy-imm-part3.sh
+++ Scripts/diy-imm-part3.sh
@@ -2,7 +2,8 @@
 # 修改默认IP
 # sed -i 's/192.168.1.1/192.168.32.1/g' package/base-files/files/bin/config_generate
 # 添加luci-app-mosdns
-rm -rf feeds/packages/net/v2ray-geodata
+find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
+find ./ | grep Makefile | grep mosdns | xargs rm -f
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 old_perms=$(stat -c %a package/mosdns/luci-app-mosdns/root/etc/init.d/mosdns)
