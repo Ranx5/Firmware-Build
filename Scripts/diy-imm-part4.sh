890eebbe34de2f17e995f6ed4252abf1683ddad4
Ran <63771479+Ranx5@users.noreply.github.com>
2024-11-30 19:39:13 +0800

    Update diy-imm-part4.sh

--- Scripts/diy-imm-part4.sh
+++ Scripts/diy-imm-part4.sh
@@ -5,11 +5,11 @@
 rm -rf feeds/packages/net/v2ray-geodata
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
-old_perms=$(stat -c %a package/mosdns/luci-app-mosdns/root/etc/init.d/mosdns)
-echo $old_perms
-rm -f package/mosdns/luci-app-mosdns/root/etc/init.d/mosdns
-wget -cO package/mosdns/luci-app-mosdns/root/etc/init.d/mosdns https://raw.githubusercontent.com/Ranx5/Firmware-Build/main/Configs/mosdns
-chmod $old_perms package/mosdns/luci-app-mosdns/root/etc/init.d/mosdns
+# old_perms=$(stat -c %a package/mosdns/luci-app-mosdns/root/etc/init.d/mosdns)
+# echo $old_perms
+# rm -f package/mosdns/luci-app-mosdns/root/etc/init.d/mosdns
+# wget -cO package/mosdns/luci-app-mosdns/root/etc/init.d/mosdns https://raw.githubusercontent.com/Ranx5/Firmware-Build/main/Configs/mosdns
+# chmod $old_perms package/mosdns/luci-app-mosdns/root/etc/init.d/mosdns
 # 添加luci-app-openclash
 rm -rf feeds/luci/applications/luci-app-openclash
 git clone --depth=1 https://github.com/vernesong/OpenClash.git -b dev package/luci-app-openclash
