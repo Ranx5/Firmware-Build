9ff51d8cc1a1d301625968dcc734f51e8cdc5d01
Ran <63771479+Ranx5@users.noreply.github.com>
2024-12-28 15:30:08 +0800

    Update diy-rk3399-part2.sh

--- Scripts/diy-rk3399-part2.sh
+++ Scripts/diy-rk3399-part2.sh
@@ -1,5 +1,8 @@
 
 #!/bin/bash
+# 添加luci-app-mosdns
+#rm -rf feeds/packages/net/v2ray-geodata
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 # 添加luci-app-openclash
 rm -rf feeds/luci/applications/luci-app-openclash
 git clone --depth=1 https://github.com/vernesong/OpenClash.git -b dev package/luci-app-openclash
