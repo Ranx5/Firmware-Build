89af4d1ff5c57d686da2584d88e9f1815cde35ba
Ran <63771479+Ranx5@users.noreply.github.com>
2024-12-30 21:35:56 +0800

    Update diy-rk3399-part2.sh

--- Scripts/diy-rk3399-part2.sh
+++ Scripts/diy-rk3399-part2.sh
@@ -9,10 +9,10 @@ git clone --depth=1 https://github.com/vernesong/OpenClash.git -b dev package/lu
 # 添加luci-app-mihomo
 git clone --depth=1 https://github.com/morytyann/OpenWrt-mihomo.git -b main package/luci-app-mihomo
 # 添加luci-app-passwall
-rm -rf feeds/luci/applications/luci-app-passwall
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall.git -b main package/luci-app-passwall
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages.git -b main package/passwall-packages
+# rm -rf feeds/luci/applications/luci-app-passwall
+# git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall.git -b main package/luci-app-passwall
+# git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages.git -b main package/passwall-packages
 # 添加luci-app-passwall2
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2.git -b main package/luci-app-passwall2
+# git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2.git -b main package/luci-app-passwall2
 # 添加luci-app-amlogic
 git clone --depth=1 https://github.com/ophub/luci-app-amlogic.git -b main package/luci-app-amlogic
