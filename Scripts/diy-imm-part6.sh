af8b6e1f369ea30126ce553dc0601e3e58ccae7b
Ran <63771479+Ranx5@users.noreply.github.com>
2025-01-09 21:51:53 +0800

    Update diy-imm-part6.sh

--- Scripts/diy-imm-part6.sh
+++ Scripts/diy-imm-part6.sh
@@ -9,6 +9,6 @@ git clone --depth=1 https://github.com/vernesong/OpenClash.git -b dev package/lu
 # 添加luci-app-mihomo
 git clone --depth=1 https://github.com/morytyann/OpenWrt-mihomo.git -b main package/luci-app-mihomo
 # 添加luci-app-passwall2
-rm -rf feeds/packages/net/{chinadns*,hysteria,geoview,trojan*,xray*,v2ray*,sing*, mosdns}
+rm -rf feeds/packages/net/{chinadns*,hysteria,geoview,trojan*,xray*,v2ray*,sing*,mosdns}
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages.git -b main package/passwall-packages
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2.git -b main package/luci-app-passwall2
