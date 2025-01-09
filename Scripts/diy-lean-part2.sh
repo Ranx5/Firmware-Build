e367ee6870df11c6e89e513f5dc1cfb407e86963
Ran <63771479+Ranx5@users.noreply.github.com>
2025-01-09 21:02:33 +0800

    Update diy-lean-part2.sh

--- Scripts/diy-lean-part2.sh
+++ Scripts/diy-lean-part2.sh
@@ -6,6 +6,7 @@ rm -rf feeds/luci/applications/luci-app-openclash
 git clone --depth=1 https://github.com/vernesong/OpenClash.git -b dev package/luci-app-openclash
 # 添加luci-app-passwall2
 rm -rf feeds/packages/net/{chinadns*,hysteria,geoview,trojan*,xray*,v2ray*,sing*, mosdns}
+rm -rf feeds/luci/applications/luci-app-passwall2
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages.git -b main package/passwall-packages
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2.git -b main package/luci-app-passwall2
 # 添加luci-app-amlogic
