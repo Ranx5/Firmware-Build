94d425f10513152a55c286a010f9139b495b30b8
Ran <63771479+Ranx5@users.noreply.github.com>
2025-01-11 16:06:04 +0800

    Create diy-rockchip-lede-part2.sh

---
+++ Scripts/diy-rockchip-lede-part2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# 添加luci-app-openclash
+git clone --depth=1 https://github.com/vernesong/OpenClash.git -b dev package/luci-app-openclash
+# 添加luci-app-passwall2
+rm -rf feeds/packages/net/{chinadns*,hysteria,geoview,trojan*,xray*,v2ray*,sing*}
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages.git -b main package/passwall-packages
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2.git -b main package/luci-app-passwall2
+# 添加luci-app-amlogic
+git clone --depth=1 https://github.com/ophub/luci-app-amlogic.git -b main package/luci-app-amlogic
