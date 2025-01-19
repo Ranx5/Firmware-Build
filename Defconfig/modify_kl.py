778cd0c0101439cc6b4232037b5896f9c750d094
Ran <63771479+Ranx5@users.noreply.github.com>
2025-01-19 23:12:22 +0800

    Create modify_kl.py

---
+++ Defconfig/modify_kl.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python3
+import yaml
+import os
+
+with open('/etc/openclash/config/kl.yaml', 'rb') as f:
+    x = yaml.safe_load(f)
+Proxy = {'HK':[], 'SG':[], 'JP':[], 'TW':[], 'KR':[], 'US':[], 'OT':[], 'All':[]}
+testtime='60'
+n = len(Proxy)
+ProxySet = set()
+pgs = []
+for p in x['proxies']:
+    name = p['name']
+    Proxy['All'].append(name)
+    if 'HK' in name:
+        Proxy['HK'].append(name)
+        ProxySet.add('HK')
+    elif 'JP' in name:
+        Proxy['JP'].append(name)
+        ProxySet.add('JP')
+    elif 'SG' in name:
+        Proxy['SG'].append(name)
+        ProxySet.add('SG')
+    elif 'TW' in name:
+        Proxy['TW'].append(name)
+        ProxySet.add('TW')
+    elif 'KR' in name:
+        Proxy['KR'].append(name)
+        ProxySet.add('KR')
+    elif 'US' in name:
+        Proxy['US'].append(name)
+        ProxySet.add('US')
+    else:
+        Proxy['OT'].append(name)
+        ProxySet.add('OT')
+ProxySet = list(ProxySet)
+Strategy1 = ['Google', 'DisneyPlus', 'Netflix', 'OpenAI']
+Strategy2 = ['Instagram', 'YouTube', 'GitHub', 'Twitter', 'Telegram']
+Strategy3 = ['Spotify', 'Microsoft']
+pgs.append({'name':'Proxy', 'type':'select', 'proxies':ProxySet+Proxy['All']})
+for s in Strategy1:
+    pgs.append({'name':s, 'type':'select', 'proxies':Proxy['All']})
+for s in Strategy2:
+    pgs.append({'name':s, 'type':'select', 'proxies':ProxySet})
+for s in Strategy3:
+    pgs.append({'name':s, 'type':'select', 'proxies':ProxySet + ['DIRECT']})
+for s in ProxySet:
+    if s != 'OT':
+        pgs.append({'name':s, 'type': 'load-balance', 'strategy': 'consistent-hashing', 'disable-udp': False,
+                    'proxies':Proxy[s], 'url': 'http://www.gstatic.com/generate_204', 'interval': testtime})
+if Proxy['OT']:
+    pgs.append({'name':'OT', 'type': 'select', 'proxies':Proxy['OT']})
+    
+rps = {}
+rps['ProxyGFW'] = {'type': 'http', 'behavior': 'classical', 'path':'./rule_provider/ProxyGFW.yaml',
+                           'url':'https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/Providers/ProxyGFWlist.yaml'}
+
+rs = []
+rs.append('GEOIP,private,DIRECT,no-resolve')
+rs.append('GEOIP,cloudflare,Proxy,no-resolve')
+rs.append('GEOSITE,cloudflare,Proxy')
+rs.append('GEOIP,telegram,Telegram,no-resolve')
+rs.append('GEOSITE,twitter,Twitter')
+rs.append('GEOIP,twitter,Twitter,no-resolve')
+rs.append('GEOSITE,instagram,Instagram')
+rs.append('GEOSITE,facebook,Instagram')
+rs.append('GEOSITE,youtube,YouTube')
+rs.append('DOMAIN-SUFFIX,googleapis.cn,Google')
+rs.append('GEOSITE,google,Google')
+rs.append('GEOSITE,spotify,Spotify')
+rs.append('GEOSITE,github,GitHub')
+rs.append('GEOSITE,openai,OpenAI')
+rs.append('GEOSITE,microsoft,Microsoft')
+rs.append('GEOSITE,disney,DisneyPlus')
+rs.append('GEOSITE,netflix,Netflix')
+rs.append('GEOIP,netflix,Netflix,no-resolve')
+rs.append('GEOSITE,category-scholar-cn,DIRECT')
+rs.append('RULE-SET,ProxyGFW,Proxy')
+rs.append('GEOIP,CN,DIRECT,no-resolve')
+rs.append('GEOSITE,apple,DIRECT')
+rs.append('DOMAIN-SUFFIX,ip.sb,GitHub')
+rs.append('DOMAIN-SUFFIX,ipify.org,YouTube')
+rs.append('MATCH,Proxy')
+z = {}
+for k in x.keys():
+    if k == 'proxy-groups':
+        z[k] = pgs
+    elif k == 'rules':
+        z[k] = rs 
+    elif k == 'proxies':    
+        z[k] = x[k]
+z['rule-providers'] = rps
+z['sniffer'] = {
+        'enable': True,
+        'force-dns-mapping': True,
+        'parse-pure-ip': True,
+        'override-destination': True,
+        'sniff': {
+            'HTTP': {
+                'ports': [80, '8080-8880'],
+                'override-destination': True
+            },
+            'TLS': {
+                'ports': [443, 8443]
+            },
+            'QUIC': {
+                'ports': [443, 8443]
+            }
+        },
+        'force-domain': [
+            '+.v2ex.com'
+        ],
+        'skip-domain': [
+            'Mijia Cloud'
+        ]
+    }
+z['dns'] = {'default-nameserver': ['223.5.5.5', '119.29.29.29'],
+            'proxy-server-nameserver': ['https://dns.alidns.com/dns-query', 'https://doh.pub/dns-query'],
+            'respect-rules': True}
+
+if os.path.exists('/etc/openclash/config/config_kl.yaml'):
+    os.system('rm /etc/openclash/config/config_kl.yaml')
+    print('删除旧配置！')
+with open('/etc/openclash/config/config_kl.yaml', 'w') as file:
+    file.write(yaml.dump(z, allow_unicode=True))
+    print('配置修改成功！')
