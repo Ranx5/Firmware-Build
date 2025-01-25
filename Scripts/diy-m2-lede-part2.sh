c190519e47b28b81379779d17f072c2432dd3368
Ran <63771479+Ranx5@users.noreply.github.com>
2025-01-25 21:08:10 +0800

    Create diy-m2-lede-part2.sh

---
+++ Scripts/diy-m2-lede-part2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# 添加luci-app-mosdns
+rm -rf feeds/packages/net/mosdns
+git clone https://github.com/sbwml/luci-app-mosdns -b v5-lua package/mosdns
+# 添加luci-app-openclash
+rm -rf feeds/luci/applications/luci-app-openclash
+git clone --depth=1 https://github.com/vernesong/OpenClash.git -b dev package/luci-app-openclash
+# 添加luci-app-passwall2
+rm -rf feeds/packages/net/{chinadns*,hysteria,geoview,trojan*,xray*,v2ray*,sing*}
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages.git -b main package/passwall-packages
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2.git -b main package/luci-app-passwall2
