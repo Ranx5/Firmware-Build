0634973b7a5bd975993dc6e6ab72da5e942f91ce
Ran <63771479+Ranx5@users.noreply.github.com>
2025-01-25 22:07:35 +0800

    Update diy-m2-lede-part2.sh

--- Scripts/diy-m2-lede-part2.sh
+++ Scripts/diy-m2-lede-part2.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-# 添加luci-app-mosdns
-rm -rf feeds/packages/net/mosdns
-git clone https://github.com/sbwml/luci-app-mosdns -b v5-lua package/mosdns
 # 添加luci-app-openclash
 rm -rf feeds/luci/applications/luci-app-openclash
 git clone --depth=1 https://github.com/vernesong/OpenClash.git -b dev package/luci-app-openclash
