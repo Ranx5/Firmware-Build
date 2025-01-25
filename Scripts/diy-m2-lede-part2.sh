ca4b1f6255a83beba5ec4e21ad7beb023286d9be
Ran <63771479+Ranx5@users.noreply.github.com>
2025-01-25 22:08:24 +0800

    Update diy-m2-lede-part2.sh

--- Scripts/diy-m2-lede-part2.sh
+++ Scripts/diy-m2-lede-part2.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # 添加luci-app-openclash
-rm -rf feeds/luci/applications/luci-app-openclash
 git clone --depth=1 https://github.com/vernesong/OpenClash.git -b dev package/luci-app-openclash
