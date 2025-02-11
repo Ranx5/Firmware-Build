a6be6d9498918e8187288787e526381cef9d6d7e
Ran <63771479+Ranx5@users.noreply.github.com>
2025-02-11 21:31:46 +0800

    Update diy-mt7981-imm-part2.sh

--- Scripts/diy-mt7981-imm-part2.sh
+++ Scripts/diy-mt7981-imm-part2.sh
@@ -7,5 +7,5 @@ git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 # 添加luci-app-openclash
 rm -rf feeds/luci/applications/luci-app-openclash
 git clone --depth=1 https://github.com/vernesong/OpenClash.git -b dev package/luci-app-openclash
-# 添加luci-app-mihomo
-git clone --depth=1 https://github.com/morytyann/OpenWrt-mihomo.git -b main package/luci-app-mihomo
+# 添加luci-app-nikki
+git clone --depth=1 https://github.com/nikkinikki-org/OpenWrt-nikki.git -b main package/luci-app-nikki
