75498fa07131e3576538ed72ae6481b6a941b455
Ran <63771479+Ranx5@users.noreply.github.com>
2025-02-11 21:17:19 +0800

    Update diy-rockchip-imm-part2.sh

--- Scripts/diy-rockchip-imm-part2.sh
+++ Scripts/diy-rockchip-imm-part2.sh
@@ -8,8 +8,8 @@ git clone --depth=1 https://github.com/vernesong/OpenClash.git -b dev package/lu
 # 添加luci-app-mihomo
 git clone --depth=1 https://github.com/morytyann/OpenWrt-mihomo.git -b main package/luci-app-mihomo
 # 添加luci-app-passwall2
-rm -rf feeds/packages/net/{chinadns*,hysteria,geoview,trojan*,xray*,v2ray*,sing*}
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages.git -b main package/passwall-packages
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2.git -b main package/luci-app-passwall2
+# rm -rf feeds/packages/net/{chinadns*,hysteria,geoview,trojan*,xray*,v2ray*,sing*}
+# git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages.git -b main package/passwall-packages
+# git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2.git -b main package/luci-app-passwall2
 # 添加luci-app-amlogic
 git clone --depth=1 https://github.com/ophub/luci-app-amlogic.git -b main package/luci-app-amlogic
