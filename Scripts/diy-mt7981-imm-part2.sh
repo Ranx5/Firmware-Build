26244bb17135236a6a6e43d1c36c896ce50c4831
Ran <63771479+Ranx5@users.noreply.github.com>
2025-06-15 13:58:33 +0800

    Update diy-mt7981-imm-part2.sh

--- Scripts/diy-mt7981-imm-part2.sh
+++ Scripts/diy-mt7981-imm-part2.sh
@@ -7,5 +7,6 @@ git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 # 添加luci-app-openclash
 rm -rf feeds/luci/applications/luci-app-openclash
 git clone --depth=1 https://github.com/vernesong/OpenClash.git -b dev package/luci-app-openclash
+sed -i 's/--set=llvm\.download-ci-llvm=true/--set=llvm.download-ci-llvm=if-unchanged/' feeds/packages/lang/rust/Makefile
 # 添加luci-app-nikki
 git clone --depth=1 https://github.com/nikkinikki-org/OpenWrt-nikki.git -b main package/luci-app-nikki
