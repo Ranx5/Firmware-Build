2dff23dc9a4ced82de63376f63aebbbd575c5e32
Ran <63771479+Ranx5@users.noreply.github.com>
2025-08-23 23:39:16 +0800

    Update mihomo_uv.py

--- Defconfig/mihomo_uv.py
+++ Defconfig/mihomo_uv.py
@@ -1,80 +1,67 @@
-
 #!/usr/bin/env python3
 import yaml
 import os
 
-with open('/etc/mihomo/run/config.yaml', 'rb') as f:
+with open('./config.yaml', 'rb') as f:
     x = yaml.safe_load(f)
-Proxy = ['HK', 'SGP', 'JP', 'TW', 'USA', 'OT']
-HK = []
-SGP = []
-JP = []
-TW = []
-USA = []
-OT = []
-testtime='300'
+Proxy = {'HK':[], 'SG':[], 'JP':[], 'TW':[], 'US':[], 'OT':[], 'All':[]}
+testtime='60'
 n = len(Proxy)
+ProxySet = set()
+pgs = []
 for p in x['proxies']:
     name = p['name']
-    if '香' in name:
-        Proxy.append(name)
-        HK.append(name)
-    elif '日' in name:
-        Proxy.append(name)
-        JP.append(name)
-    elif '新' in name:
-        Proxy.append(name)
-        SGP.append(name)
-    elif '台' in name:
-        Proxy.append(name)
-        TW.append(name)
-    elif '美' in name:
-        Proxy.append(name)
-        USA.append(name)
-    else:
-        Proxy.append(name)
-        OT.append(name)
-pgs = []
-pgs.append({'name':'Proxy', 'type':'select', 'proxies':Proxy})
-pgs.append({'name':'Google', 'type':'select', 'proxies':Proxy[n:]})
-pgs.append({'name':'DisneyPlus', 'type':'select', 'proxies':Proxy[n:]})
-pgs.append({'name':'Netflix', 'type':'select', 'proxies':Proxy[n:]})
-pgs.append({'name':'OpenAI', 'type':'select', 'proxies':Proxy[n:]})
-pgs.append({'name':'Instagram', 'type':'select', 'proxies':Proxy[:n]})
-pgs.append({'name':'YouTube', 'type':'select', 'proxies':Proxy[:n]+['DIRECT']})
-pgs.append({'name':'Spotify', 'type':'select', 'proxies':Proxy[:n]})
-pgs.append({'name':'GitHub', 'type':'select', 'proxies':Proxy[:n]})
-pgs.append({'name':'Twitter', 'type':'select', 'proxies':Proxy[:n]})
-pgs.append({'name':'Telegram', 'type':'select', 'proxies':Proxy[:n]})
-pgs.append({'name':'Microsoft', 'type':'select', 'proxies':Proxy[:n]+['DIRECT']})
-pgs.append({'name':'DNS', 'type':'select', 'proxies':Proxy})
-pgs.append({'name':'HK', 'type': 'load-balance', 'strategy': 'consistent-hashing', 'disable-udp': False,
-            'proxies':HK, 'url': 'http://www.gstatic.com/generate_204', 'interval': testtime})
-pgs.append({'name':'SGP', 'type': 'load-balance', 'strategy': 'consistent-hashing', 'disable-udp': False,
-            'proxies':SGP, 'url': 'http://www.gstatic.com/generate_204', 'interval': testtime})
-pgs.append({'name':'TW', 'type': 'load-balance', 'strategy': 'consistent-hashing', 'disable-udp': False,
-            'proxies':TW, 'url': 'http://www.gstatic.com/generate_204', 'interval': testtime})
-pgs.append({'name':'JP', 'type': 'load-balance', 'strategy': 'consistent-hashing', 'disable-udp': False,
-            'proxies':JP, 'url': 'http://www.gstatic.com/generate_204', 'interval': testtime})
-pgs.append({'name':'USA', 'type': 'load-balance', 'strategy': 'consistent-hashing', 'disable-udp': False,
-            'proxies':USA, 'url': 'http://www.gstatic.com/generate_204', 'interval': testtime})
-pgs.append({'name':'OT', 'type': 'select', 'proxies':OT})
+    if '稳定' in name:
+        Proxy['All'].append(name)
+        if 'HK' in name:
+            Proxy['HK'].append(name)
+            ProxySet.add('HK')
+        elif 'JP' in name:
+            Proxy['JP'].append(name)
+            ProxySet.add('JP')
+        elif 'SG' in name:
+            Proxy['SG'].append(name)
+            ProxySet.add('SG')
+        elif 'TW' in name:
+            Proxy['TW'].append(name)
+            ProxySet.add('TW')
+        elif 'US' in name:
+            Proxy['US'].append(name)
+            ProxySet.add('US')
+        else:
+            Proxy['OT'].append(name)
+            ProxySet.add('OT')
+ProxySet = list(ProxySet)
+Strategy1 = ['Google', 'DisneyPlus', 'Netflix', 'OpenAI']
+Strategy2 = ['Instagram', 'YouTube', 'GitHub', 'Twitter', 'Telegram']
+Strategy3 = ['Spotify', 'Microsoft']
+pgs.append({'name':'Proxy', 'type':'select', 'proxies':ProxySet+Proxy['All']})
+for s in Strategy1:
+    pgs.append({'name':s, 'type':'select', 'proxies':Proxy['All']})
+for s in Strategy2:
+    pgs.append({'name':s, 'type':'select', 'proxies':ProxySet+Proxy['All']})
+for s in Strategy3:
+    pgs.append({'name':s, 'type':'select', 'proxies':ProxySet + ['DIRECT']})
+for s in ProxySet:
+    if s != 'OT':
+        pgs.append({'name':s, 'type': 'load-balance', 'strategy': 'consistent-hashing', 'disable-udp': False,
+                    'proxies':Proxy[s], 'url': 'http://www.gstatic.com/generate_204', 'interval': testtime})
+if Proxy['OT']:
+    pgs.append({'name':'OT', 'type': 'select', 'proxies':Proxy['OT']})
 rps = {}
-rps['ProxyGFW'] = {'type': 'http', 'behavior': 'classical', 'path':'./rule_provider/ProxyGFW.yaml',
-                           'url':'https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/Providers/ProxyGFWlist.yaml'}
-
+rps['Apple'] = {'type': 'http', 'behavior': 'ipcidr', 'path':'./rule_provider/Apple.yaml',
+                           'url':'https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/refs/heads/meta/geo-lite/geoip/apple.yaml'}
 rs = []
 rs.append('GEOIP,private,DIRECT,no-resolve')
-rs.append('IP-SUFFIX,1.1.1.1/24,DNS,no-resolve')
-rs.append('IP-SUFFIX,8.8.8.8/24,DNS,no-resolve')
+rs.append('GEOIP,cloudflare,Proxy,no-resolve')
+rs.append('GEOSITE,cloudflare,Proxy')
 rs.append('GEOIP,telegram,Telegram,no-resolve')
 rs.append('GEOSITE,twitter,Twitter')
-rs.append('GEOIP,twitter,Twitter,no-resolve')
 rs.append('GEOSITE,instagram,Instagram')
 rs.append('GEOSITE,facebook,Instagram')
 rs.append('GEOSITE,youtube,YouTube')
-rs.append('DOMAIN-SUFFIX,googleapis.cn,Google')
 rs.append('GEOSITE,google,Google')
+rs.append('GEOIP,google,Google,no-resolve')
 rs.append('GEOSITE,spotify,Spotify')
 rs.append('GEOSITE,github,GitHub')
 rs.append('GEOSITE,openai,OpenAI')
@@ -82,12 +69,10 @@ rs.append('GEOSITE,microsoft,Microsoft')
 rs.append('GEOSITE,disney,DisneyPlus')
 rs.append('GEOSITE,netflix,Netflix')
 rs.append('GEOIP,netflix,Netflix,no-resolve')
-rs.append('GEOSITE,category-scholar-cn,DIRECT')
-rs.append('RULE-SET,ProxyGFW,Proxy')
-rs.append('GEOIP,CN,DIRECT,no-resolve')
 rs.append('GEOSITE,apple,DIRECT')
-rs.append('DOMAIN-SUFFIX,ip.sb,GitHub')
-rs.append('DOMAIN-SUFFIX,ipify.org,YouTube')
+rs.append('RULE-SET,Apple,DIRECT,no-resolve')
+rs.append('GEOSITE,cn,DIRECT')
+rs.append('GEOIP,CN,DIRECT,no-resolve')
 rs.append('MATCH,Proxy')
 z = {}
 for k in x.keys():
@@ -98,37 +83,13 @@ for k in x.keys():
     elif k == 'proxies':    
         z[k] = x[k]
 z['rule-providers'] = rps
-z['sniffer'] = {
-        'enable': True,
-        'force-dns-mapping': True,
-        'parse-pure-ip': True,
-        'override-destination': True,
-        'sniff': {
-            'HTTP': {
-                'ports': [80, '8080-8880'],
-                'override-destination': True
-            },
-            'TLS': {
-                'ports': [443, 8443]
-            },
-            'QUIC': {
-                'ports': [443, 8443]
-            }
-        },
-        'force-domain': [
-            '+.v2ex.com'
-        ],
-        'skip-domain': [
-            'Mijia Cloud'
-        ]
-    }
 z['dns'] = {'default-nameserver': ['223.5.5.5', '119.29.29.29'],
             'proxy-server-nameserver': ['https://dns.alidns.com/dns-query', 'https://doh.pub/dns-query'],
             'respect-rules': True}
 
-if os.path.exists('/etc/mihomo/profiles/config_uv.yaml'):
-    os.system('rm /etc/mihomo/profiles/config_uv.yaml')
+if os.path.exists('/etc/nikki/profiles/config_uv.yaml'):
+    os.system('rm /etc/nikki/profiles/config_uv.yaml')
     print('删除旧配置！')
-with open('/etc/mihomo/profiles/config_uv.yaml', 'w') as file:
+with open('/etc/nikki/profiles/config_uv.yaml', 'w') as file:
     file.write(yaml.dump(z, allow_unicode=True))
     print('配置修改成功！')
