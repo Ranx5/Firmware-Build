30f04cdbc32a7092e3c2b69c70bbe80f5f691680
Ran <63771479+Ranx5@users.noreply.github.com>
2025-09-09 21:24:14 +0800

    Create diy-rockship-imm-part2.sh

---
+++ Scripts/diy-rockship-imm-part2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# 添加luci-app-mosdns
+rm -rf feeds/packages/net/mosdns
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+# 添加luci-app-openclash
+rm -rf feeds/luci/applications/luci-app-openclash
+git clone --depth=1 https://github.com/vernesong/OpenClash.git -b dev package/luci-app-openclash
+# 添加luci-app-nikki
+git clone --depth=1 https://github.com/nikkinikki-org/OpenWrt-nikki.git -b main package/luci-app-nikki
