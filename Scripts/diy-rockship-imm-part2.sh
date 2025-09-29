0e1a30dad698b8b4840b3c23856fea9f6355f9fe
Ran <63771479+Ranx5@users.noreply.github.com>
2025-09-29 22:51:54 +0800

    Update diy-rockship-imm-part2.sh

--- Scripts/diy-rockship-imm-part2.sh
+++ Scripts/diy-rockship-imm-part2.sh
@@ -7,3 +7,12 @@ rm -rf feeds/luci/applications/luci-app-openclash
 git clone --depth=1 https://github.com/vernesong/OpenClash.git -b dev package/luci-app-openclash
 # 添加luci-app-nikki
 git clone --depth=1 https://github.com/nikkinikki-org/OpenWrt-nikki.git -b main package/luci-app-nikki
+# 更改Rust
+mkdir rust
+git clone https://github.com/immortalwrt/packages.git -b master rust
+cd rust
+git checkout 10862df850ae012b34ec9c57a9005b1f7e1e2aca
+cd ..
+rm -rf feeds/packages/lang/rust
+cp -rf rust/lang/rust feeds/packages/lang/rust
+rm -rf rust
