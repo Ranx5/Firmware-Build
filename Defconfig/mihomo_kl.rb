d360c10b93aabab6d2a880aee4a223a43ca0cc19
Ran <63771479+Ranx5@users.noreply.github.com>
2026-03-30 21:17:33 +0800

    Create mihomo_kl.rb

---
+++ Defconfig/mihomo_kl.rb
@@ -0,0 +1,82 @@
+#!/usr/bin/env ruby
+require 'yaml'
+
+source = "config.yaml"
+output = "profiles/config_kl.yaml"
+
+data = YAML.load_file(source, aliases: true)
+
+HK = data["proxies"].select { |n| n["name"].include?("香港") }.map { |n| n["name"]}
+TW = data["proxies"].select { |n| n["name"].include?("台湾")}.map { |n| n["name"] }
+JP = data["proxies"].select { |n| n["name"].include?("日本")}.map { |n| n["name"] }
+SG = data["proxies"].select { |n| n["name"].include?("新加坡")}.map { |n| n["name"] }
+US = data["proxies"].select { |n| n["name"].include?("美国")}.map { |n| n["name"] }
+KR = data["proxies"].select { |n| n["name"].include?("韩国")}.map { |n| n["name"] }
+node_name = data["proxies"].map { |n| n["name"] }
+
+Strategy1 = ['Google', 'DisneyPlus', 'Netflix', 'OpenAI']
+Strategy2 = ['Instagram', 'YouTube', 'GitHub', 'Twitter', 'Telegram']
+Strategy3 = ['Spotify', 'Microsoft']
+
+Proxy = ["HK", "TW", "JP", "SG", "US", "KR"]
+ProxySet = {"HK" => HK, "TW" => TW, "JP" => JP, "SG" => SG, "US" => US, "KR" => KR}
+
+proxy_groups = [{"name" => "Proxy", "type" => "select", "proxies" => Proxy + node_name}]
+
+Strategy1.each do |group|
+  proxy_groups << {"name" => group, "type" => "select", "proxies" => node_name}                         
+end
+
+Strategy2.each do |group|
+  proxy_groups << {"name" => group, "type" => "select", "proxies" => Proxy}
+end
+
+Strategy3.each do |group|
+  proxy_groups << {"name" => group, "type" => "select", "proxies" => Proxy + ["DIRECT"]}
+end
+
+Proxy.each do |group|
+  proxy_groups << {"name" => group,
+                        "type" => "load-balance",
+                        "strategy" => "consistent-hashing",
+                        "url" => "consistent-hashing",
+                        "interval" => "300",
+                        "disable-udp" => false,
+                        "proxies" => ProxySet[group]}
+end
+config = {}
+config["dns"] = {
+                        "default-nameserver" => ["223.5.5.5", "119.29.29.29"],
+                        "proxy-server-nameserver" => ["https://dns.alidns.com/dns-query", "https://doh.pub/dns-query"],
+                        "respect-rules" => true
+}
+config["proxies"] = data["proxies"]
+config["proxy-groups"] = proxy_groups
+config["rule-providers"] = {"Apple" => {"type" => "http", "behavior" => "ipcidr", "path" => "./rule_provider/Apple.yaml", 
+                            "url" => "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/refs/heads/meta/geo-lite/geoip/apple.yaml"}}
+config["rules"] = [
+                        "GEOIP,private,DIRECT,no-resolve",
+                        "GEOIP,cloudflare,Proxy,no-resolve",
+                        "GEOSITE,cloudflare,Proxy",
+                        "GEOIP,telegram,Telegram,no-resolve",
+                        "GEOSITE,twitter,Twitter",
+                        "GEOSITE,instagram,Instagram",
+                        "GEOSITE,facebook,Instagram",
+                        "GEOSITE,youtube,YouTube",
+                        "GEOSITE,google,Google",
+                        "GEOIP,google,Google,no-resolve",
+                        "GEOSITE,spotify,Spotify",
+                        "GEOSITE,github,GitHub",
+                        "GEOSITE,openai,OpenAI",
+                        "GEOSITE,microsoft,Microsoft",
+                        "GEOSITE,disney,DisneyPlus",
+                        "GEOSITE,netflix,Netflix",
+                        "GEOIP,netflix,Netflix,no-resolve",
+                        "GEOSITE,apple,DIRECT",
+                        "RULE-SET,Apple,DIRECT,no-resolve",
+                        "GEOSITE,cn,DIRECT",
+                        "GEOIP,CN,DIRECT,no-resolve",
+                        "MATCH,Proxy"
+]
+
+File.write(output, config.to_yaml)
