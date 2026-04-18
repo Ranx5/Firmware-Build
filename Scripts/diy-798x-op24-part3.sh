aad4742c37d8b126cb773eb164e4a9dd8851a079
Ran <63771479+Ranx5@users.noreply.github.com>
2026-04-18 13:59:53 +0800

    Delete Scripts/diy-798x-op24-part3.sh

--- Scripts/diy-798x-op24-part3.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cp -af ./feeds/mtk_openwrt_feed/24.10/files/* .
-for file in $(find ./feeds/mtk_openwrt_feed/24.10/patches-base -name "*.patch" | sort); do patch -f -p1 -i ${file}; done
