89ccb697ddd3c6c9f02fcc1f40ea2fdd2f81e878
Ran <63771479+Ranx5@users.noreply.github.com>
2026-06-15 20:24:50 +0800

    Update diy-flippy-part2.sh

--- Scripts/diy-flippy-part2.sh
+++ Scripts/diy-flippy-part2.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+# 添加luci-app-daed
+rm -rf feeds/packages/net/daed
+git clone --depth=1 https://github.com/QiuSimons/luci-app-daed package/dae
 # 添加luci-app-mosdns
 rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 26.x feeds/packages/lang/golang
+git clone --depth=1 https://github.com/sbwml/packages_lang_golang -b 26.x feeds/packages/lang/golang
 rm -rf feeds/packages/net/mosdns
-git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone --depth=1 https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 # 添加luci-app-openclash
 rm -rf feeds/luci/applications/luci-app-openclash
 git clone --depth=1 https://github.com/vernesong/OpenClash.git -b dev package/luci-app-openclash
